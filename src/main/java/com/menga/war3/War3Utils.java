72ce415b205883c63c6da6fef7b86a846c2b51b0
MenciusCheng <295567172@qq.com>
2018-06-05 00:09:05 +0800

    update war3

--- src/main/java/com/menga/war3/War3Utils.java
+++ src/main/java/com/menga/war3/War3Utils.java
@@ -10,6 +10,52 @@ public class War3Utils {
     public final static Integer BASE_HERO_HP = 500;
     public final static Double BASE_HERO_ATTACK_DELAY = 1.0;
 
+    // 护甲减伤因子
+    public final static Double DEFENSE_ARMOR = 0.06;
+
+    public static void main(String[] args) {
+        Integer hAttack = 150;
+        Integer hDamage = getDamageByAttackDuration(hAttack, 0.5, 3);
+        Integer mArmor = 2;
+        Integer mHp = getHpByArmor(mArmor, hDamage);
+        System.out.println("hero_attack=" + hAttack + ", monster_armor=" + mArmor + ", monster_hp=" + mHp);
+
+        Integer mAttack = 50;
+        Integer mDamage = getDamageByAttackDuration(mAttack, 1.0, 60);
+        Integer hArmor = 15;
+        Integer hHp = getHpByArmor(hArmor, mDamage);
+        System.out.println("monster_attack=" + mAttack + ", hero_armor=" + hArmor + ", hero_hp=" + hHp);
+
+        Integer hBDamage = getDamageByAttackDuration(hAttack, 0.5, 60);
+        Integer bArmor = 30;
+        Integer bHp = getHpByArmor(bArmor, hBDamage);
+        Integer bAttack = getAttackByHp(hHp, hArmor, 6, 0.5);
+        System.out.println("boss_attack=" + bAttack + ", boss_armor=" + bArmor + ", boss_hp=" + bHp);
+    }
+
+    /**
+     * 承受伤害 = 生命值 * (1 + armor * 护甲减伤因子)
+     * 攻击力 = 承受伤害 / 攻击持续时间(秒) * 攻击间隔(秒)
+     */
+    public static Integer getAttackByHp(Integer hp, Integer armor, Integer duration, Double delay) {
+        Double damage = hp * (1 + armor * DEFENSE_ARMOR);
+        return (int)(damage / duration * delay);
+    }
+
+    /**
+     * 造成伤害 = 攻击力 / 攻击间隔(秒) * 攻击持续时间(秒)
+     */
+    public static Integer getDamageByAttackDuration(Integer attack, Double delay, Integer duration) {
+        return (int)(attack / delay * duration);
+    }
+
+    /**
+     *     由公式 总生命值 = 生命值 * (1 + armor * 护甲减伤因子) 得到
+     * 生命值 = 总生命值 / (1 + armor * 护甲减伤因子)
+     */
+    public static Integer getHpByArmor(Integer armor, Integer hp) {
+        return (int)(hp / (1 + armor * DEFENSE_ARMOR));
+    }
 
     /**
      * 计算攻击力对护甲减伤后的伤害
