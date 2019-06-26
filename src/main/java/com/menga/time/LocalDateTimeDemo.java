2ebbf8396fe198154fe0b9dd53e9cf7e7f217db6
程孟威 <chengmengwei@iplas.com.cn>
2019-06-26 14:19:44 +0800

    update

--- src/main/java/com/menga/time/LocalDateTimeDemo.java
+++ src/main/java/com/menga/time/LocalDateTimeDemo.java
@@ -1,7 +1,9 @@
 package com.menga.time;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * Created by Marvel on 2019/5/14.
@@ -9,10 +11,13 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeDemo {
 
     public static void main(String[] args) {
-        LocalDateTime time = LocalDateTime.now();
-        LocalDateTime t2 = time.plusDays(3);
-        String timeStr = time.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        System.out.println(timeStr);
+        LocalDateTime t1 = LocalDateTime.now();
+        LocalDateTime t2 = t1.plusDays(3);
+
         System.out.println(t2.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
+        System.out.println(t2.toLocalDate());
+        System.out.println(LocalTime.of(18, 0));
+
+        System.out.println(LocalDateTime.of(t2.toLocalDate(), LocalTime.of(18, 0)));
     }
 }
