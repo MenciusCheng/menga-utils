ff548ae3c6083e142fcaf8a0bc68d5910bd84661
程孟威 <295567172@qq.com>
2019-10-28 17:16:30 +0800

    update

--- src/main/java/com/menga/time/LocalDateTimeDemo.java
+++ src/main/java/com/menga/time/LocalDateTimeDemo.java
@@ -1,7 +1,9 @@
 package com.menga.time;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -12,7 +14,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
  */
 public class LocalDateTimeDemo {
 
-    public static void main(String[] args) {
+    public static void test1() {
         LocalDateTime t1 = LocalDateTime.now();
         LocalDateTime t2 = t1.plusDays(3).minusHours(2);
 
@@ -24,5 +26,23 @@ public class LocalDateTimeDemo {
 
         LocalTime t3 = LocalTime.now();
         System.out.println(t3.format(DateTimeFormatter.ofPattern("HH:mm")));
+    }
+
+    public static void testStringToDate() {
+        DateTimeFormatter yyyyMMddDTF = DateTimeFormatter.ofPattern("yyyyMMdd");
+        DateTimeFormatter hhmmssDTF = DateTimeFormatter.ofPattern("HHmmss");
+
+        LocalDate localDate = LocalDate.parse("20191028", yyyyMMddDTF);
+        LocalTime localTime = LocalTime.parse("164020", hhmmssDTF);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+
+        System.out.println(localDate);
+        System.out.println(localTime);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    }
+
+    public static void main(String[] args) {
+        testStringToDate();
     }
 }
