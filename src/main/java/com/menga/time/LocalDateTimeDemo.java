d5b93be2628b83693e931120f1253b9692809d9d
程孟威 <295567172@qq.com>
2019-11-13 10:26:35 +0800

    update

--- src/main/java/com/menga/time/LocalDateTimeDemo.java
+++ src/main/java/com/menga/time/LocalDateTimeDemo.java
@@ -42,7 +42,41 @@ public class LocalDateTimeDemo {
         System.out.println(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
     }
 
+    public static void test() {
+        String createdTimeStr = "20191112T215921.000Z";
+        LocalDateTime createdTime = toLocalDateTime(createdTimeStr);
+        LocalDateTime startTime = createdTime.plusSeconds(3600);
+        LocalDateTime endTime = startTime.plusSeconds(14400);
+        String remainingTime = timeBetween(System.currentTimeMillis(), endTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        System.out.println(createdTime);
+        System.out.println(startTime);
+        System.out.println(endTime);
+        System.out.println(remainingTime);
+    }
+
+    private static LocalDateTime toLocalDateTime(String time) {
+        String ts = time.substring(0, 8) + time.substring(9, 15);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime localDateTime = LocalDateTime.parse(ts, dtf);
+        return localDateTime.plusHours(7);
+    }
+
+    private static String timeBetween(Long start, Long end) {
+        int between = (int)((end - start) / 1000);
+        int hour = between / 3600;
+        int minute = (between - hour * 3600) / 60;
+
+        StringBuilder sb = new StringBuilder();
+        if (hour > 0) {
+            sb.append(hour).append("小时");
+        }
+        if (minute > 0) {
+            sb.append(minute).append("分钟");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        testStringToDate();
+        test();
     }
 }
