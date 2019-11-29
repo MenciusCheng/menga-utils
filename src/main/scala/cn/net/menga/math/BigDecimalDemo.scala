92cce307ec0cabf9768efe4b7746b63801601867
程孟威 <295567172@qq.com>
2019-11-29 11:45:16 +0800

    测试浮点数模运算

--- src/main/scala/cn/net/menga/math/BigDecimalDemo.scala
+++ src/main/scala/cn/net/menga/math/BigDecimalDemo.scala
@@ -7,11 +7,25 @@ import scala.math.BigDecimal.RoundingMode
   */
 object BigDecimalDemo {
 
-  def main(args: Array[String]): Unit = {
+  def test1(): Unit = {
     val a = BigDecimal(9321.5)
 
     val b = a.setScale(-2, RoundingMode.HALF_UP)
 
     System.out.print(b)
+  }
+
+  def test2(): Unit = {
+    val a = BigDecimal(6.33)
+    val b = BigDecimal(0.3)
+    val c = a % b
+    val (c1, c2) = a /% b
+    System.out.println(c)
+    System.out.println(c1)
+    System.out.println(c2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    test2()
   }
 }
